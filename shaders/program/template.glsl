e98f9a011f8a25ee780d4fad072ed4462fdad491
EternalMC <55027815+EternalMC@users.noreply.github.com>
2020-03-02 09:48:45 -0600

    Update template.glsl

--- shaders/program/template.glsl
+++ shaders/program/template.glsl
@@ -1,1 +1,30 @@
 
+#version 450 compatibility
+#define program name ie composite
+#define fsh / vsh
+#define ShaderStage ie -1 0 1 2 
+#include "/lib/Syntax.glsl"
+#include "/lib/Settings.glsl"
+
+// TODOs go here and notes
+
+/* DRAWBUFFERS:0 */ // drawbuffers
+
+// uniforms
+uniform sampler2D colortex0;
+uniform sampler2D colortex1;
+
+// in out
+in vec2 texcoord;
+
+// fragdata layout (  layout (location = (0/1/2)) out vec4 (name of fragdata);  )
+layout (location = 0) out vec4 albedo;
+
+// Debug
+#include "/lib/Debug.glsl"
+
+void main() {
+    albedo = texture(colortex0, texcoord);
+    
+    exit();
+}
