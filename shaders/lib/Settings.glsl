b7a1cf9b3d026230f3423232116ce3b53a8d7866
EternalMC <55027815+EternalMC@users.noreply.github.com>
2020-03-02 11:35:51 -0600

    Update Settings.glsl

--- shaders/lib/Settings.glsl
+++ shaders/lib/Settings.glsl
@@ -6,10 +6,15 @@ const int colortex2Format = RGBA32F;
 const bool colortex2Clear = false;
 
 
-
 */
 
-//Adjustable variables for composite2. Tune these for performance
+//Composite0
+//todo
+
+//Composite1.
+//todo
+
+//Composite2. Tune these for performance
 #define MAX_RAY_STEPS           30
 #define RAY_STEP_LENGTH         0.1
 #define RAY_DEPTH_BIAS          0.05
@@ -17,6 +22,13 @@ const bool colortex2Clear = false;
 #define NUM_RAYS                1  // [1 2 4 8 16 32]
 #define NUM_BOUNCES             2
 
+//Composite3
+//todo
+
+//Composite4
+//todo
+
+//Shadow
 const int shadowMapResolution = 2048;  // [1024 2048 3072 4096 8192]
 
 #define SHADOW_MAP_BIAS 0.80           // [0.00 0.60 0.70 0.80 0.85 0.90]
