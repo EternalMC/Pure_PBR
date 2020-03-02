0680631cdd5ebc072cee79b4435d767422ce6327
EternalMC <55027815+EternalMC@users.noreply.github.com>
2020-03-02 17:23:45 -0600

    Update camera.glsl

--- shaders/lib/Camera/camera.glsl
+++ shaders/lib/Camera/camera.glsl
@@ -1,37 +1,26 @@
 //todo e
 stuct camera {
 
+}/*
+* Get an exposure using the Saturation-based Speed method.
+*/
+float getSaturationBasedExposure(float aperture,
+                                 float shutterSpeed,
+                                 float iso)
+{
+    float l_max = (7800.0f / 65.0f) * Sqr(aperture) / (iso * shutterSpeed);
+    return 1.0f / l_max;
 }
-
-vec3 CalculateBokeh(vec2 screenCoord, vec2 offset) {
-            const float lod  = 2.0;
-            const float lod2 = exp2(lod);
-
-            const float a = tau / LENS_BLADES;
-            const mat2 rot = mat2(cos(a), -sin(a), sin(a), cos(a));
-
-            const vec3 size  = 0.4 * vec3(1.0 - vec2(LENS_SHIFT, LENS_SHIFT * 0.5), 1.0);
-            const vec3 size0 = size * LENS_SHARPNESS;
-            const vec3 size1 = size * LENS_SHARPNESS * 0.8;
-
-            vec2 coord = (screenCoord - offset) * lod2;
-
-            float r = 0.0;
-
-            const vec2 caddv = vec2(sin(LENS_ROTATION), -cos(LENS_ROTATION));
-            vec2 addv = caddv;
-
-            vec2 centerOffset = coord - 0.5;
-
-            for(int i = 0; i < LENS_BLADES; ++i) {
-                addv = rot * addv;
-                r = max(r, dot(addv, centerOffset));
-            }
-
-            r = mix(r, fLength(centerOffset) * 0.8, LENS_ROUNDING);
-
-            vec3 bokeh = saturate(1.0 - smoothstep(size0, size, vec3(r)));
-                 bokeh = bokeh * (1.0 - saturate(smoothstep(size, size1, vec3(r)) * LENS_BIAS));
-
-            return bokeh;
-        }
+ 
+/*
+* Get an exposure using the Standard Output Sensitivity method.
+* Accepts an additional parameter of the target middle grey.
+*/
+float getStandardOutputBasedExposure(float aperture,
+                                     float shutterSpeed,
+                                     float iso,
+                                     float middleGrey = 0.18f)
+{
+    float l_avg = (1000.0f / 65.0f) * Sqr(aperture) / (iso * shutterSpeed);
+    return middleGrey / l_avg;
+}
